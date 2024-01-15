ff058e4f5fa09801f6b7f48d0dd7ac2660c0b735
Dika <dikamaah@gmail.com>
2024-01-15 17:44:44 +0700

    Update interface and model

--- frontend/lib/presentation/pages/analyze_skin/models/analyze_skin_model.dart
+++ frontend/lib/presentation/pages/analyze_skin/models/analyze_skin_model.dart
@@ -20,9 +20,11 @@ class DetectionResponse {
 
 class PredictionData {
   final List<Prediction> predictions;
+  final List<SkinProblem> skinProblems;
 
   PredictionData({
     required this.predictions,
+    required this.skinProblems,
   });
 
   factory PredictionData.fromJson(Map<String, dynamic> json) {
@@ -31,26 +33,80 @@ class PredictionData {
         .map((prediction) => Prediction.fromJson(prediction))
         .toList();
 
-    return PredictionData(predictions: parsedPredictions);
+    List<SkinProblem> skinProblemsList = [];
+    if (parsedPredictions.isNotEmpty) {
+      for (var prediction in parsedPredictions) {
+        String name = prediction.name;
+
+        var found = false;
+        for (var skinProblem in skinProblemsList) {
+          if (skinProblem.name == name) {
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          skinProblemsList.add(SkinProblem(name: name));
+        }
+      }
+    }
+
+    return PredictionData(
+        predictions: parsedPredictions, skinProblems: skinProblemsList);
   }
 }
 
+class SkinProblem {
+  final String name;
+
+  SkinProblem({
+    required this.name,
+  });
+}
+
 class Prediction {
-  final double confidenceLevel;
-  final String label;
-  final int status;
+  final double confidence;
+  final String name;
+  final List<Coordinate> coords;
 
   Prediction({
-    required this.confidenceLevel,
-    required this.label,
-    required this.status,
+    required this.confidence,
+    required this.name,
+    required this.coords,
   });
 
   factory Prediction.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> coordsList = json['coords'] ?? [];
+    final List<Coordinate> parsedCoords =
+        coordsList.map((coord) => Coordinate.fromJson(coord)).toList();
+
     return Prediction(
-      confidenceLevel: json['confidence level'] ?? 0.0,
-      label: json['label'] ?? '',
-      status: json['status'] ?? 0,
+      confidence: json['confidence'] ?? 0.0,
+      name: json['name'] ?? '',
+      coords: parsedCoords,
+    );
+  }
+}
+
+class Coordinate {
+  final double x1;
+  final double y1;
+  final double x2;
+  final double y2;
+
+  Coordinate({
+    required this.x1,
+    required this.y1,
+    required this.x2,
+    required this.y2,
+  });
+
+  factory Coordinate.fromJson(Map<String, dynamic> json) {
+    return Coordinate(
+      x1: json['x1'] ?? 0.0,
+      y1: json['y1'] ?? 0.0,
+      x2: json['x2'] ?? 0.0,
+      y2: json['y2'] ?? 0.0,
     );
   }
 }
